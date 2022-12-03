b78b2579e511abccbdc1fc49d974be6bd900621e
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2022-12-03 17:04:22 +0700

    Cập nhật lại layout.hbs và các hbs liên quan

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
 const controller = {} //Để {} vì là object có thể chứa thêm các hàm khác
 
 controller.show = async (req, res) => {
-    res.render('index');
+    res.render('infoTaiKhoan');
 }
 
 module.exports = controller;
