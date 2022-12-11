6048bf23964e1fd088d5945bebf54926ad26d749
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2022-12-11 22:00:45 +0700

    chức năng lọc giá, lọc nhà xe, lọc loại xe, sort theo giá tăng dần, giá giảm dần, theo giờ khởi hành sớm và trễ nhất

--- controllers/search-tripController.js
+++ controllers/search-tripController.js
@@ -1,79 +1,87 @@
 const controller = {}; //Để {} vì là object có thể chứa thêm các hàm khác
 const models = require("../models");
+const sequelize = require("sequelize");
 
 controller.show = async (req, res) => {
   let dataSearch = req.query;
-  // let page = req.query.page || 1;
+  let sort = req.query.sort || "earliest";
+  let minPrice = req.query.min || 0;
+  let maxPrice = req.query.max || 2000000;
+  let orders = {
+    earliest: ["startTime", "ASC"],
+    latest: ["startTime", "DESC"],
+    cheapest: ["price", "ASC"],
+    expensive: ["price", "DESC"],
+  };
+
+  let page = req.query.page || 1;
   let limit = 3;
-  // let offset = (page - 1) * limit;
-  res.locals.searchStart = await models.ChuyenXe.findAll({
-    attributes: [
-      "startProvince",
-    ],
-    group: ["startProvince"],
-  });
-  res.locals.searchEnd = await models.ChuyenXe.findAll({
-    attributes: [
-      "endProvince",
-    ],
-    group: ["endProvince"],
-  });
-  if (dataSearch.date) {
-    let date = dataSearch.date.trim();
-    res.locals.chuyenxes = await models.ChuyenXe.findAll({
-      include: [
-        {
-          model: models.NhaXe,
-          include: [models.Review],
-        },
-        {
-          model: models.LoaiXe,
-        },
-      ],
-      where: {
-        startProvince: dataSearch.start,
-        endProvince: dataSearch.end,
-        startDate: date,
+
+  let option = {
+    include: [
+      {
+        model: models.NhaXe,
+        include: [models.Review],
+        where: {},
       },
-      limit: limit,
-      // offset: offset,
-    });
-  }
-  else {
-    res.locals.chuyenxes = await models.ChuyenXe.findAll({
-      include: [
-        {
-          model: models.NhaXe,
-          include: [models.Review],
-        },
-        {
-          model: models.LoaiXe,
-        },
-      ],
-      where: {
-        startProvince: dataSearch.start,
-        endProvince: dataSearch.end,
+      {
+        model: models.LoaiXe,
+        where: {},
       },
-      limit: limit,
-      // offset: offset,
-    });
+    ],
+    order: [],
+    where: {},
+    limit: limit,
+    offset: (page - 1) * limit,
+  };
+
+  option.where.price = {
+    [sequelize.Op.between]: [minPrice, maxPrice],
+  };
+
+  if (sort) {
+    option.order.push(orders[sort]);
   }
 
+  if (dataSearch.start || dataSearch.end || dataSearch.date) {
+    option.where.startProvince = dataSearch.start;
+    option.where.endProvince = dataSearch.end;
+    // option.where.startTime = {
+    //   [sequelize.Op.gte]: dataSearch.date,
+    // };
+  }
+  if (req.query.nhaxe) {
+    let arrNhaxe = req.query.nhaxe.split(",");
+    option.include[0].where.id = {
+      [sequelize.Op.in]: arrNhaxe,
+    };
+  }
+  if (req.query.loaixe) {
+    let arrLoaixe = req.query.loaixe.split(",");
+    option.include[1].where.id = {
+      [sequelize.Op.in]: arrLoaixe,
+    };
+  }
 
-  // let count = await models.ChuyenXe.findAndCountAll()
+  let { rows, count } = await models.ChuyenXe.findAndCountAll(option);
 
-  // res.locals.pagination = {
-  //     page: page,
-  //     // pageCount: Math.ceil(count.count / limit),
-  //     limit: limit,
-  //     offset: offset,
-  //     count: count.count,
-  // }
+  res.locals.chuyenxes = rows;
+  res.locals.pagination = {
+    page: page,
+    limit: limit,
+    totalRows: count / 2,
+  };
 
-  res.locals.nhaxes = await models.NhaXe.findAll();
-  res.locals.loaixes = await models.LoaiXe.findAll();
+  res.locals.nhaxes = await models.NhaXe.findAll({
+    include: [models.ChuyenXe],
+  });
+  res.locals.loaixes = await models.LoaiXe.findAll({
+    include: [models.ChuyenXe],
+  });
+
+  console.log(res.locals.chuyenxes);
+
   res.render("search-trip", { dataSearch });
 };
-
 
 module.exports = controller;
--- routes/search-tripRoute.js
+++ routes/search-tripRoute.js
@@ -8,6 +8,7 @@ const controller1 = require("../controllers/xacnhanController");
 const controller2 = require("../controllers/thongtinkhachhangController");
 const controller3 = require("../controllers/thanhtoanController");
 const userController = require('../controllers/userController')
+
 Router.get("/:id", tripInfoController.show);
 Router.get("/", controller.show);
 Router.get("/:id/thanh-toan/xacnhan", userController.isLoggedIn, controller1.show);
